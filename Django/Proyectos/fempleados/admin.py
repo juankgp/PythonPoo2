aa6027ef9b3d06e41fe0bdf009881b2a0d7a6c47
juankgp <jcfenix01@gmail.com>
2020-08-25 20:54:27 -0500

    nuevos clases

--- Django/Proyectos/fempleados/admin.py
+++ Django/Proyectos/fempleados/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from fempleados.models import *
 # Register your models here.
+admin.site.register(Empleados)
+
+admin.site.register(Prestamos)
+
+admin.site.register(Departamentos)
--- Django/Proyectos/fempleados/models.py
+++ Django/Proyectos/fempleados/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Empleados(models.Model):
+    Apellido = models.CharField(max_length=25)
+    Nombres = models.CharField( max_length=25)
+    Cedula = models.CharField(max_length=14)
+    FechaNacimiento = models.DateField()
+    
+    def NombreCompleto(self):
+        cadena = "{0} {1}, {2}"
+        return cadena.format(self.Apellido, self.Nombres)
+
+        def __str__(self):
+            NombreCompleto = "{0},{1}"
+            return self.NombreCompleto()
+
+
+class Prestamos(models.Model):
+    fecha = models.DateField()
+    valor = models.DecimalField(max_digits=3, decimal_places=2)
+    plazo = models.CharField(max_length=25)
+    estado = models.BooleanField(default = True)
+                
+    def __str__(self):
+        return "{0} {1}".format(self.valor, self.plazo)
+
+class Departamentos(models.Model):
+    nombreDep = models.CharField(max_length=25)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "{0} ({1})".format(self.nombreDep,self.estado)
+    
